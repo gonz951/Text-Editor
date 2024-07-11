e928d942a92a0e8923f61b7f9352dae1bc8f7ec2
Fel <felgonzalez951@gmail.com>
2024-07-11 15:46:40 -0600

    starting the configuration of the webpack plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,9 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+// * Working on this
 // TODO: Add CSS loaders and babel to webpack.
+// ! Nothing yet
 
 module.exports = () => {
   return {
@@ -18,7 +20,17 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+
+      new WebpackPwaManifest({
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Just a text editor.',
+        // need to add other elements
+      })
     ],
 
     module: {
