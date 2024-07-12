215e4bf2a30168cf769ebb52d3c3adcc2cbcae3c
Fel <felgonzalez951@gmail.com>
2024-07-12 12:27:44 -0600

    added getDb method code, still need to make putDb method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    // ? Need to figure out put method
+  } catch (err) {
+    console.error('putDb not implemented');
+  }
 
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const jateDb = await openDB('jate', 1);
+
+    const text = jateDb.transaction('jate', 'readonly');
+
+    const store = text.objectStore('jate');
+
+    const request = store.getAll();
+
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+  } catch (err) {
+    console.error('getDb not implemented');
+  }
+}
 
 initdb();
