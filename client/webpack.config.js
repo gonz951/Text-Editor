584dbc46784ed821c32df861840c9d2fc4c7d57b
Fel <felgonzalez951@gmail.com>
2024-07-15 13:10:34 -0600

    added module rules to export options, still need to finish plugin code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,6 +25,11 @@ module.exports = () => {
         title: 'JATE'
       }),
 
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
       new WebpackPwaManifest({
         name: 'JATE',
         short_name: 'JATE',
@@ -35,7 +40,21 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'];
+            },
+          },
+        },
       ],
     },
   };
